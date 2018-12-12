4a497cb3b9cd598956a747e33c7d7e98cfa7f4b7
Josh Grazda <joshuagrazda@yahoo.com>
2018-12-12 00:55:21 -0500

    Full metro train built with props and Doors open and close with scene manager

---
+++ EmergingMedia_Final/Assets/Scripts/DoorHolder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHolder : MonoBehaviour 
+{
+	[Header("Door Toggle")]
+	public bool openDoors;
+	public bool closeDoors;
+
+	[SerializeField]
+	private List<GameObject> doors;
+
+	void Update ()
+	{
+		if(openDoors)
+		{
+        	OpenAllDoors();
+			openDoors = false;
+		}
+		if(closeDoors)
+		{
+			CloseAllDoors();
+			closeDoors = false;
+		}
+	}
+
+	void OpenAllDoors()
+	{
+		for(int i = 0; i < doors.Count ; i++)
+		{
+			doors[i].GetComponent<DoorsOpen>().OpenDoors();
+		}
+	}
+
+	void CloseAllDoors()
+	{
+		for(int i = 0; i < doors.Count ; i++)
+		{
+			doors[i].GetComponent<DoorsOpen>().CloseDoors();
+		}
+	}
+}
---
+++ EmergingMedia_Final/Assets/Scripts/DoorsOpen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoorsOpen : MonoBehaviour 
+{
+	[SerializeField]
+	private bool rightDoor;
+
+	public void OpenDoors()
+	{
+		if(rightDoor)
+			transform.DOMoveZ(transform.position.z - 1.5f, 2f, false);
+		else
+			transform.DOMoveZ(transform.position.z + 1.5f, 2f, false);
+	}
+
+	public void CloseDoors()
+	{
+		if(rightDoor)
+			transform.DOMoveZ(transform.position.z + 1.5f, 2f, false);
+		else
+			transform.DOMoveZ(transform.position.z - 1.5f, 2f, false);
+	}
+}
